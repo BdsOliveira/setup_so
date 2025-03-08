f00f807f3b86f34b61e47a2b99dd88cac8472a64
Bruno Oliveira <bds.commus@gmail.com>
2025-03-08 12:11:54 -0300

    add icon theme zip

--- ubuntu/setup_ubuntu.sh
+++ ubuntu/setup_ubuntu.sh
@@ -16,9 +16,13 @@ gnome-shell-extension-manager -y
 unzip -o ./ubuntu/assets/gnome-extensions.zip -d $HOME/.local/share/gnome-shell/
 
 # Install GTK Themes
-
 mkdir -p $HOME/.themes
 
 unzip -o ./ubuntu/assets/GTK-Themes.zip -d $HOME/.themes/
 
 ln -sf $HOME/.themes/Orchis-Dark/gtk-4.0/{assets,gtk.css,gtk-dark.css} $HOME/.config/gtk-4.0/
+
+# Install Icon Theme
+mkdir -p $HOME/.local/share/icons
+
+unzip -o ./ubuntu/assets/icon-themes.zip -d $HOME/.local/share/icons
