ac1aab0127e0ba1e802ea9cffcecc50d0a1e6d01
Bruno Oliveira <bds.commus@gmail.com>
2025-03-08 11:22:07 -0300

    first commit

---
+++ setup_ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
+
+sudo apt install curl \
+rsync \
+git \
+gdebi \
+nautilus-admin \
+nautilus-extension-gnome-terminal \
+sassc \
+gnome-tweaks \
+gnome-shell-extension-manager -y
+
+
