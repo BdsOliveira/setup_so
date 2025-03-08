ccba62a8dc0926cdb5516b345b79a03011d9f908
Bruno Oliveira <bds.commus@gmail.com>
2025-03-08 12:06:05 -0300

    add symbol link

--- ubuntu/setup_ubuntu.sh
+++ ubuntu/setup_ubuntu.sh
@@ -20,3 +20,5 @@ unzip -o ./ubuntu/assets/gnome-extensions.zip -d $HOME/.local/share/gnome-shell/
 mkdir -p $HOME/.themes
 
 unzip -o ./ubuntu/assets/GTK-Themes.zip -d $HOME/.themes/
+
+ln -sf $HOME/.themes/Orchis-Dark/gtk-4.0/{assets,gtk.css,gtk-dark.css} $HOME/.config/gtk-4.0/
