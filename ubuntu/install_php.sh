9fe02b6ba4da73eff1e8ebdabfa60ec959d42049
BdsOliveira <bds.commus@gmail.com>
2025-03-20 13:55:10 -0300

    complete php installation

--- ubuntu/install_php.sh
+++ ubuntu/install_php.sh
@@ -8,10 +8,10 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 
 sudo apt-get install php7.4 php7.4-fpm -y
-sudo apt-get install php7.4-common php7.4-mysql php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl php7.4-intl php7.4-zip php7.4-bcmath -y
+sudo apt-get install php7.4-common php7.4-mysql php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl php7.4-intl php7.4-zip php7.4-sqlite3 php7.4-dom php7.4-imagick php7.4-bcmath -y
 
 sudo apt-get install php8.4 php8.4-fpm -y
-sudo apt-get install php8.4-common php8.4-mysql php8.4-mbstring php8.4-xml php8.4-gd php8.4-curl php8.4-intl php8.4-zip php8.4-bcmath -y
+sudo apt-get install php8.4-common php8.4-mysql php8.4-mbstring php8.4-xml php8.4-gd php8.4-curl php8.4-intl php8.4-zip php8.4-sqlite3 php8.4-dom php8.4-imagick php8.4-bcmath -y
 
 echo "alias phpv='sudo update-alternatives --config php'" >> ~/.bash_aliases
 echo "alias php74='sudo update-alternatives --set php /usr/bin/php7.4'" >> ~/.bash_aliases
@@ -25,3 +25,7 @@ php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
 sudo mv composer.phar /usr/local/bin/composer
+
+composer global config bin-dir --absolute
+
+echo 'export PATH="$PATH:/home/$USER/.config/composer/vendor/bin"' >> ~/.bashrc
