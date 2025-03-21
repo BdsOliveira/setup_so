f70ad0786043797e2b7ee367b3366474767cf4c2
BdsOliveira <bds.commus@gmail.com>
2025-03-21 19:54:40 -0300

    add alias file

---
+++ utils/alias.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "alias update='sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y'" >> ~/.bash_aliases
+echo "alias cu='composer update'" >> ~/.bash_aliases
+echo "alias cda='composer dumpautoload'" >> ~/.bash_aliases
+echo "alias pas='php artisan serve'" >> ~/.bash_aliases
+echo "alias pam='php artisan migrate'" >> ~/.bash_aliases
+echo "alias pams='php artisan migrate --seed'" >> ~/.bash_aliases
+echo "alias gac='git add . && git commit -m'" >> ~/.bash_aliases
+echo "alias n16='nvm use 16'" >> ~/.bash_aliases
+echo "alias n22='nvm use 22'" >> ~/.bash_aliases
+echo "alias y='yarn'" >> ~/.bash_aliases
+echo "alias ys='yarn serve'" >> ~/.bash_aliases
+
+source ~/.bash_aliases
+source ~/.bashrc
