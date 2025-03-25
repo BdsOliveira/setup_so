f4d7063819e5ea441380a96e18734a2b4c3c9f48
bdsoliveira <bds.commus@gmail.com>
2025-03-25 11:07:24 -0300

    Update install_php.sh

--- ubuntu/install_php.sh
+++ ubuntu/install_php.sh
@@ -7,10 +7,12 @@ sudo apt install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 
-sudo apt-get install php7.4 php7.4-fpm -y
+sudo apt-get --purge remove php7.4-common
+sudo apt-get install php7.4 php7.4-fpm php7.4-cli -y
 sudo apt-get install php7.4-common php7.4-mysql php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl php7.4-intl php7.4-zip php7.4-sqlite3 php7.4-dom php7.4-imagick php7.4-bcmath -y
 
-sudo apt-get install php8.4 php8.4-fpm -y
+sudo apt-get --purge remove php8.4-common
+sudo apt-get install php8.4 php8.4-fpm php8.4-cli -y
 sudo apt-get install php8.4-common php8.4-mysql php8.4-mbstring php8.4-xml php8.4-gd php8.4-curl php8.4-intl php8.4-zip php8.4-sqlite3 php8.4-dom php8.4-imagick php8.4-bcmath -y
 
 echo "alias phpv='sudo update-alternatives --config php'" >> ~/.bash_aliases
