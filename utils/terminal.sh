d5f2b671b1afbf229ed678bca79d95bb1bd77a01
bdsoliveira <bds.commus@gmail.com>
2025-04-23 15:01:30 -0300

    Update terminal.sh

--- utils/terminal.sh
+++ utils/terminal.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+acho "alias ll='ls -las'" >> ~/.bash_aliases
+acho "alias phpv='sudo update-alternatives --config php'" >> ~/.bash_aliases
 echo "alias up='sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y'" >> ~/.bash_aliases
 echo "alias cu='composer update'" >> ~/.bash_aliases
+echo "alias cr='composer require'" >> ~/.bash_aliases
 echo "alias cda='composer dumpautoload'" >> ~/.bash_aliases
 echo "alias pa='php artisan'" >> ~/.bash_aliases
+echo "alias pat='php artisan test'" >> ~/.bash_aliases
 echo "alias pas='php artisan serve'" >> ~/.bash_aliases
 echo "alias pam='php artisan migrate'" >> ~/.bash_aliases
 echo "alias pams='php artisan migrate --seed'" >> ~/.bash_aliases
+echo "alias pamfs='php artisan migrate:fresh --seed'" >> ~/.bash_aliases
+echo "alias pamrs='php artisan migrate:refresh --seed'" >> ~/.bash_aliases
 echo "alias gac='git add . && git commit -m'" >> ~/.bash_aliases
+echo "alias gpush='git push origin \$(git branch --show-current)'" >> ~/.bash_aliases
+echo "alias gpull='git pull origin \$(git branch --show-current)'" >> ~/.bash_aliases
 echo "alias n16='nvm use 16'" >> ~/.bash_aliases
 echo "alias n22='nvm use 22'" >> ~/.bash_aliases
 echo "alias y='yarn'" >> ~/.bash_aliases
 echo "alias ys='yarn serve'" >> ~/.bash_aliases
-echo "alias gpush='git push origin \$(git branch --show-current)'" >> ~/.bash_aliases
 
 # isntall zsh
 sudo apt install zsh
