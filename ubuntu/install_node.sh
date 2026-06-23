10076878dcf53fcd76c217a5974bedf9e4d26ba7
Bruno Oliveira <bds.commus@gmail.com>
2026-06-23 18:50:43 -0300

    Update Node.js installation to version 24
    Update Node.js installation to use version 24 and remove version 22. Adjust verification commands for Node.js and npm versions.

--- ubuntu/install_node.sh
+++ ubuntu/install_node.sh
@@ -4,22 +4,21 @@
 sudo apt update && sudo apt upgrade -y
 
 # Download and install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
 
 # in lieu of restarting the shell
 \. "$HOME/.nvm/nvm.sh"
 
 # Download and install Node.js:
-nvm install 22
-nvm install 20
+nvm install 24
 nvm install 16
 
 # Verify the Node.js version:
-node -v # Should print "v22.14.0".
-nvm current # Should print "v22.14.0".
+node -v # Should print "v24.17.0".
+nvm current # Should print "v16.20.2".
 
 # Verify npm version:
-npm -v # Should print "10.9.2".
+npm -v # Should print "11.13.0".
 
 # Atualiza o bash
 source ~/.bashrc
